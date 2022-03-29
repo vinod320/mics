65c2e810e3477ff0f4672757fd9b354ee907de74
Vinod Reddy <vinodreddy320@gmail.com>
2022-03-29 11:05:20 +0530

    updates

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -14,7 +14,7 @@ return(
     <h3 className="contentAbout">Director - Business Development</h3>
     <h3 className="contentAbout">M.Tech(Mechanical)</h3>
     <h3 className="contentAbout">Experience: 8+ years</h3>
-    <h3 className="contentAbout">info@mics.com</h3>
+    <h3 className="contentAbout">bharath@mics.co.in, info@mics.com</h3>
     <h3 className="contentAbout">+91-9703095576</h3>
     </div>
     <div className="top">
@@ -25,6 +25,10 @@ return(
     <h3 className="contentAbout">maarsinspectorate@gmail.com</h3>
     <h3 className="contentAbout">+91-9908230413</h3>
     </div>
+    </div>
+    <div className="top">
+    <h1 className="headerContactTop">Contact Address</h1>
+    <h1 className="headerContactBottom">Lig-97, Ground floor, Near hospital lane, KPHB colony, Kukatpally, Medchal-Malkajgiri district, Hyderabad-500072</h1>
     </div>
     </>
 )
